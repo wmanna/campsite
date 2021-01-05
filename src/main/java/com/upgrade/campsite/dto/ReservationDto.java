894e2f68cc9ee8d4ef0728c9b193ce746cdf78db
Walter M <walter_manna@yahoo.com.ar>
2021-01-04 21:46:06 -0300

    Changed: schema.sql was moved to database folder.

--- src/main/java/com/upgrade/campsite/dto/ReservationDto.java
+++ src/main/java/com/upgrade/campsite/dto/ReservationDto.java
@@ -2,11 +2,11 @@ package com.upgrade.campsite.dto;
 
 public class ReservationDto {
 
-    private String id;
-    private String code;
+    private String reservationCode;
     private String reservationDate;
     private String arrivalDate;
     private String departureDate;
     private String userEmailAddress;
+    private String userFullName;
 
 }
